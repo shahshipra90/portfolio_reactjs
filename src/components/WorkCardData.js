e1d205aa3eee3ccf856baae6713ec49d977f8f16
shahshipra90 <shahshipr90@gmail.com>
2024-07-06 13:13:25 -0400

    New project weather dashboard added

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,34 +1,40 @@
-import pro1 from "../assets/project1.jpeg"
-import pro2 from "../assets/project2.jpeg"
-import pro3 from "../assets/project3.jpg"
-import pro4 from "../assets/pro4.jpg"
+import pro1 from "../assets/project1.jpeg";
+import pro2 from "../assets/project2.jpeg";
+import pro3 from "../assets/project3.jpg";
+import pro4 from "../assets/pro4.jpg";
+import pro5 from "../assets/pro5.png";
 
 const ProjectCardData = [
-    {
-        imgsrc: pro4,
-        title: "Shoppers",
-        text: "An e-commerce platform for convinent purchasing of clothing and accessories.",
-        view: ""
-    },
-    {
-        imgsrc: pro1,
-        title: "Uwin Online Research Sharing Platform",
-        text: "An online platform for the faculties of Uwindsor to showcase their research work, that helps them to collabrate and get ideas from other staff memebers.",
-        view: "https://github.com/jayduttbhatt1999/CRSP"
-    },
-    {
-        imgsrc: pro2,
-        title: "Book My Adventure",
-        text: "An online platform for the enthusiast traveller for booking an adventure rides all over the nation",
-        view: "https://github.com/nidhi-1201/bookmyadventure"
-    },
-    {
-        imgsrc: pro3,
-        title: "Reclaim the Meal",
-        text: "An platform that helps the environemnt by reducing the food wastage, and helping the needy people to overcome starvation.",
-        view: "https://github.com/shahshipra90/Reclaim-The-Meal"
-    },
-
+  {
+    imgsrc: pro5,
+    title: "Weather Dashborad",
+    text: "An dashboard that shows weather forecast of 5-days and your favourite locations",
+    view: "https://github.com/shahshipra90/weather-dashboard",
+  },
+  {
+    imgsrc: pro4,
+    title: "Shoppers",
+    text: "An e-commerce platform for convinent purchasing of clothing and accessories.",
+    view: "https://github.com/shahshipra90/shoppers",
+  },
+  {
+    imgsrc: pro1,
+    title: "Uwin Online Research Sharing Platform",
+    text: "An online platform for the faculties of Uwindsor to showcase their research work, that helps them to collabrate and get ideas from other staff memebers.",
+    view: "https://github.com/jayduttbhatt1999/CRSP",
+  },
+  {
+    imgsrc: pro2,
+    title: "Book My Adventure",
+    text: "An online platform for the enthusiast traveller for booking an adventure rides all over the nation",
+    view: "https://github.com/nidhi-1201/bookmyadventure",
+  },
+  {
+    imgsrc: pro3,
+    title: "Reclaim the Meal",
+    text: "An platform that helps the environemnt by reducing the food wastage, and helping the needy people to overcome starvation.",
+    view: "https://github.com/shahshipra90/Reclaim-The-Meal",
+  },
 ];
 
 export default ProjectCardData;
